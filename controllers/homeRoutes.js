523a9fe83a22552ba59521278a04e0cdeaa1bb35
Victor <victor5055@outlook.com>
2023-01-05 17:45:29 -0500

    Added check for post updates.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,20 +3,42 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
+    try {
+      // Get all the posts and JOIN with user data
+      const postData = await Post.findAll({
+        include: [{ model: User }],
+      });
+      
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [{ model: User }, { model: Comment }],
+      });
+  
+      const post = postData.get({ plain: true });
 
-router.get('/post/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [{model: User }, { model: Comment}],
-       
-    });
-}});
-
-    const post = postData.get({ plain: true });
-
-
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in
-    });
-    
+      let match = false;
+      if (req.session.user_id == post.user_id) {
+        match = true;
+      }
+  
+      res.render('post', {
+        ...post,
+        logged_in: req.session.logged_in,
+        match
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });

fe85e514911957bc5ceb4ad4174d4216c01724c2
Victor <victor5055@outlook.com>
2023-01-05 13:27:34 -0500

    Adding POST request for post routes.

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Comment, User, Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all comments and JOIN with user data
+      const commentData = await Comment.findAll({
+        include: [{ model: User }, { model: Post }],
+      });
+  
+      //template can read data
+      const comments = commentData.map((comment) => comment.get({ plain: true }));
+      console.log(comments);
+      res.status(200);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+router.post('/', withAuth, async (req, res) => {
+    try {
+        console.log(req.body);
+        const newComment = await Comment.create({
+          ...req.body,
+          user_id: req.session.user_id, 
+        }
+  });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try { 
+
+    },
+}); 
--- public/post.js
+++ public/post.js
@@ -1,19 +1,18 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
-    const comments = document.querySelector('#comments').value.trim();
-    if (comments && event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+    const content = document.querySelector('#comments').value.trim();
+    if (content && post_id) {
       //(`Button selected. id = ${id} and comments = ${comments}`);
-      const response = await fetch(`/api/posts/${id}`, {//Need to update Pull request
-        method: 'PUT',
-        body: JSON.stringify({ comments }),
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({content, post_id}),
         headers: {
           'Content-Type': 'application/json',
         },
       });
 
       if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace(`/post/${post_id}`);
         alert('comment added');
       } else {
         alert('Failed to create comment');

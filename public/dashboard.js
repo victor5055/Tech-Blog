72d92eb178518920c4464e8e906419fa46ea2f5c
Victor <victor5055@outlook.com>
2023-01-05 13:40:02 -0500

    Dashboard and post views/controllers updated.

---
+++ public/dashboard.js
@@ -0,0 +1,42 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#post-name').value.trim();
+    const description = document.querySelector('#post-desc').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+        alert('post created');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    event.preventDefault();
+
+    if (event.target.hasAttribute('delete-id')) {
+      const id = event.target.getAttribute('delete-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.dashboard('/');
+        alert('post deleted');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };

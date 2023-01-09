bf58d573791ff84c4c5ee955db5e3bcaa4ae4bb8
Victor <victor5055@outlook.com>
2023-01-09 15:57:58 -0500

    finishing the login/logout for user.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,9 +4,11 @@ const { User } = require('../../models');
 // Creates new user
 router.post('/', async (req, res) => {
   try {
+    
     // creates object based on the request body and stores in database
     const userData = await User.create(req.body);
 
+ 
     // Set up sessions with a 'loggedIn' variable and 'userid variable 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -19,4 +21,50 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Login
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.status(200).json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// Logout
+router.post('/logout', (req, res) => {
+    //When the user logs out, eliminate the session
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+//Export router
 module.exports = router;

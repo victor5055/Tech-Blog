2999aa1138ecfc3d0b1c0c69fc3150f15e7e9d15
Victor <victor5055@outlook.com>
2023-01-09 13:24:33 -0500

    Setting up Session.

--- server.js
+++ server.js
@@ -13,3 +13,15 @@ const session = require('express-session');
 // express app
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      // Stored in milliseconds
+      maxAge: 60 * 60 * 1000, // expires after 1 hour
+    },
+    resave: false,
+    saveUninitialized: true,
+  };
+  app.use(session(sess))

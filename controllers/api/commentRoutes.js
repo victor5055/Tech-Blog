68bfa7cdf382ea0353e2ed6c7b30cf20f09f30bd
Victor <victor5055@outlook.com>
2023-01-09 15:49:46 -0500

    Helpers update.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
         include: [{ model: User }, { model: Post }],
       });
   
-      //template can read data
+      //Serialize data
       const comments = commentData.map((comment) => comment.get({ plain: true }));
       console.log(comments);
       res.status(200);
@@ -17,17 +17,38 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
 router.post('/', withAuth, async (req, res) => {
     try {
         
         const newComment = await Comment.create({
           ...req.body,
           user_id: req.session.user_id, 
-        }
-  });
+         });
 
+         res.status(200).json(newComment);
+        } catch (err) {
+          res.status(400).json(err);
+        }
+      });
   router.delete('/:id', withAuth, async (req, res) => {
     try { 
-
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
     },
 }); 
+
+if (!commentData) {
+  res.status(404).json({ message: 'No comment found with this provided id!' });
+  return;
+}
+
+res.status(200).json(commentData);
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
+module.exports = router;

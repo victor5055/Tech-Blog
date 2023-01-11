1b15df8efe6a85744fa8e44f8ace288446612bb1
Victor <victor5055@outlook.com>
2023-01-11 15:33:27 -0500

    Fixing routers index.js.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
+//Import routers for the application
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes)
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 
 //Export router

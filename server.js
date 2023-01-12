e1a11d79edba2124304238662f41c77631ac9c79
Victor <victor5055@outlook.com>
2023-01-12 17:42:47 -0500

    Running seed in heroku.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 
 // express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Set up sessions
 const sess = {

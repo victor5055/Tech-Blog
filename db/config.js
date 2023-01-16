d54b5237cfcb2a243ce39f4fd5494229c6e50d17
Victor <victor5055@outlook.com>
2023-01-16 11:44:06 -0500

    Node

--- db/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "us-cdbr-east-06.cleardb.net",
-    USER: "bc06b359960615",
-    PASSWORD: "a3f4ccb4",
-    DB: "heroku_f6875754ae08aca"
-  };
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 
 // express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
